9598818131dadfc75a9d0c427e5681e47f31a02a
Konstantin Smelov <kostya_smelov@inbox.ru>
2022-10-03 17:15:40 +0300

    ver 0.3

--- src/main/java/com/smelov/service/impl/BotServiceImpl.java
+++ src/main/java/com/smelov/service/impl/BotServiceImpl.java
@@ -11,6 +11,8 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
 
